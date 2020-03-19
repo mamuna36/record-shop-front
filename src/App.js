ad435b5b5df0625ef67d94010ba3b64b6d46ac1d
Sebastian Glaser <anx@ulzq.de>
2020-03-19 10:33:24 +0100

    add: admin/users/editor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 
 import RecordList  from './components/records/List';
+import UserEditor  from './components/users/Editor';
 import UserList    from './components/users/List';
 
 function App() {
   return <>
-    <Route path="/admin/records/" component={RecordList}/>
-    <Route path="/admin/users/"   component={UserList}/>
+    <Link to="/admin/records/">Records</Link>
+    <Link to="/admin/users/">Users</Link>
+    <Switch>
+      <Route path="/admin/records/"  component={RecordList}/>
+      <Route path="/admin/users/:id" component={UserEditor}/>
+      <Route path="/admin/users/"    component={UserList}/>
+    </Switch>
   </>;
 }
 
